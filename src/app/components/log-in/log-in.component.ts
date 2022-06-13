fed3a3e5e4f54c63a1c49e58dc86895d3f91c50f
CianMcGriskin <81272459+CianMcGriskin@users.noreply.github.com>
2022-06-13 04:59:40 +0100

    init

---
+++ src/app/components/log-in/log-in.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css',
+              './css/main.css',
+              './css/util.css',
+              './vendor/select2/select2.min.css',
+              './vendor/daterangepicker/daterangepicker.css',
+              './vendor/css-hamburgers/hamburgers.min.css',
+              './fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+              './fonts/iconic/css/material-design-iconic-font.min.css',
+              './vendor/animate/animate.css',
+              './vendor/animsition/css/animsition.min.css',
+              './vendor/bootstrap/css/bootstrap.min.css']
+})
+export class LogInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
