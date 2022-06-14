32385532bded2b7eee73ce4ade88db332ce75ecd
CianMcGriskin <81272459+CianMcGriskin@users.noreply.github.com>
2022-06-14 04:04:03 +0100

    Updated login page and routed the register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+// Routing
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+// Components
 import { LogInComponent } from './components/log-in/log-in.component';
 import { RegisterComponent } from './components/register/register.component';
-
+// Angular Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+// Angular Flex Module
 import { FlexLayoutModule } from '@angular/flex-layout';
+// Forms Module
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css',
+  '../log-in/css/main.css',
+  '../log-in/css/util.css',
+  '../log-in/vendor/select2/select2.min.css',
+  '../log-in/vendor/daterangepicker/daterangepicker.css',
+  '../log-in/vendor/css-hamburgers/hamburgers.min.css',
+  '../log-in/fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+  '../log-in/fonts/iconic/css/material-design-iconic-font.min.css',
+  '../log-in/vendor/animate/animate.css',
+  '../log-in/vendor/animsition/css/animsition.min.css',
+  '../log-in/vendor/bootstrap/css/bootstrap.min.css']
 })
 export class RegisterComponent implements OnInit {
   Roles: any = ['Admin', 'Author', 'Reader'];
